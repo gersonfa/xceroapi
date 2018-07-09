97b4466379d32dd5d4a5b32cd222fbd87d4fea8b
Gerson Flores Acuña <gerson71fa@hotmail.com>
2018-07-09 11:03:01 -0500

    update unit number

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ const sendJSONresponse = require('../shared/common').sendJSONresponse
 async function user_drivers_list(req, res, next) {
   try {
 
-    let drivers = await User.find({role: 'Driver'}, 'email full_name rating image')
+    let drivers = await User.find({role: 'Driver'}, 'email full_name rating image unit_number')
 
     sendJSONresponse(res, 200, drivers)
   } catch (e) {
