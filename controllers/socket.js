c85c76710af00506a9b048a27723fdc24ba522b2
Gerson Flores Acuña <gerson71fa@hotmail.com>
2018-08-23 17:43:40 -0500

    debug

--- controllers/socket.js
+++ controllers/socket.js
@@ -14,7 +14,7 @@ module.exports = (io, users_online) => {
 
     let check = setInterval(async () => {
       if (!socket.connected) {
-        console.log('se desconecto', user_id)
+        console.log('se desconecto', user)
         users_online.delete(user_id)
         let bases = await Base.find({stack: user_id})
 
